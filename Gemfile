1ca1e2d04983013da90e1cc49cf3e30f67da8a8f
Apple <sarah.crawley6@gmail.com>
2016-03-06 17:04:37 +0000

    CRUD complete, editing and deleting feature tests need fixing

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'paperclip'
 gem 'haml'
 gem 'simple_form'
+gem 'factory_girl_rails'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -15,6 +15,7 @@ feature 'posts' do
     scenario 'user can create a post when user clicks create post' do
       visit '/'
       click_link 'New Post'
+      # attach_file 'post[image]', Rails.root.join('spec','files', 'images','surfsup.jpg')
       attach_file('Image', "spec/files/images/surfsup.jpg")
       fill_in 'Caption', with: "france #surfing time"
       click_button 'Create Post'
@@ -22,22 +23,54 @@ feature 'posts' do
       expect(page).to have_css("img[src*='surfsup.jpg']")
     end
 
-  end
-
-  context 'deleting posts' do
-    before {Post.create caption: 'Tube'}
-
-    scenario 'user can delete a post when user clicks delete post' do
-      visit '/'
-      click_link 'Edit Post'
-      click_link 'Delete Post'
-      expect(page).not_to have_content 'Tube'
-      expect(page).to have_content 'Post successfully deleted'
+    it "won't update a post without an image" do
+      attach_file('Image', 'spec/files/dummy.zip')
+      click_button 'Update Post'
+      expect(page).to have_content("Wrong format")
     end
 
   end
 
-  # context 'editing posts' do
-  #   scenario 'a user can edit a post when user clicks eid
-  # end
-end
+  context 'displaying posts' do
+
+    scenario 'the index displays correct created job information' do
+      job_one = create(:post, caption: "This is post one")
+      job_two = create(:post, caption: "This is the second post")
+      visit '/'
+      expect(page).to have_content("This is post one")
+      expect(page).to have_content("This is the second post")
+      expect(page).to have_css("img[src*='surfsup']")
+    end
+
+  end
+
+    context'Editing posts' do
+      background do
+        job = create(:post)
+        visit '/'
+        find(:xpath, "//a[contains(@href,'posts/1')]").click
+        click_link 'Edit Post'
+      end
+      scenario 'Can edit a post' do
+        fill_in 'Caption', with: "lalala!"
+        click_button 'Update Post'
+        expect(page).to have_content("Post updated")
+        expect(page).to have_content("lalala!")
+      end
+    end
+
+    context 'Deleting posts' do
+      background do
+        post = create(:post, caption: 'gogo')
+        visit '/'
+        find(:xpath, "//a[contains(@href,'posts/1')]").click
+        click_link 'Edit Post'
+      end
+      scenario 'Can delete a single post' do
+        click_link 'Delete Post'
+        expect(page).to have_content('Post deleted')
+        expect(page).to_not have_content('gogo')
+      end
+    end
+
+  end
